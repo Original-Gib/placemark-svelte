5428ad3cd95957d4ccb970fcabd911b9893b705e
Adam Gibson <adamhenrygibson@gmail.com>
2023-05-19 21:14:48 +0100

    prepped for deployment

--- src/lib/PlacemarkList.svelte
+++ src/lib/PlacemarkList.svelte
@@ -1,16 +1,22 @@
 <script>
+	//imports
 	import { onMount } from 'svelte';
 	import { placemarkService } from '../services/placemark-service';
 	import { page } from '$app/stores';
 
+	// variable to get the category id from the page url
 	const categoryId = $page.params.categoryId;
 
+	//variable for the placemark list array
 	let placemarkList = [];
+
+	// on mount function calls the get placemarks function from the placemark services file and passes in the category id
 	onMount(async () => {
 		placemarkList = await placemarkService.getPlacemarks(categoryId);
 	});
 </script>
 
+<!-- table to display the placemark list  -->
 <table class="table is-fullwidth is-hoverable">
 	<thead>
 		<th>Name</th>
